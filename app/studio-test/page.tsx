7f9ccee413f0e47f7d98b82d3809c084bf7fb2dc
Deniz Tulay <denizlevent@Mac.fritz.box>
2026-01-08 23:07:32 +0100

    fix: remove studio-test page to resolve build error

--- app/studio-test/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function StudioTest() {
-  return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
-      <h1>Studio Test Route</h1>
-      <p>Wenn du das siehst, funktionieren die Routes grundsätzlich!</p>
-    </div>
-  );
-}
-

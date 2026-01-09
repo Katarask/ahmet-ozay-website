3eabe5a988a2747286ad4aabcfd5bba4f330200e
Deniz Tulay <denizlevent@PCMUC20.eura.local>
2026-01-09 16:41:17 +0100

    fix: add comment schema to schemaTypes index for Studio visibility

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import article from '../schemas/article'
+import comment from '../schemas/comment'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [article],
+  types: [article, comment],
 }
